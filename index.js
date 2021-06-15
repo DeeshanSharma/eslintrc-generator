b1be06eb003a9f918da08fa1d2d64c8f9bf5f200
Deeshan Sharma <deeshansharma04@gmail.com>
2021-06-15 19:11:35 +0530

    ✨ feat: create .eslintrc.json file using config

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
 const paintTerminal = require('./lib/terminal');
+const generateFile = require('./lib/generateFile');
 
 paintTerminal();
+generateFile().then((packages) => console.log(packages));
